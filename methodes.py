55b4684d13820ff131ec7281de57bfc59dce5ecd
stefano <72141994+D261324@users.noreply.github.com>
2022-09-19 11:35:21 +0200

    rewrite huidig bestand methodes.py
    er ging iets fout met de commit. moest dit even terug zetten om het weer goed te kunnen zetten

--- methodes.py
+++ methodes.py
@@ -1,24 +1,34 @@
 def encoding ():
     print("we starten het proces van encoderen...") 
-    inputImgName = input("hoe heet het volledige bestand + extentie die je wil gebruiken: ")
+    
+    inputImgName = "blabla"
+    noExtentionProvided = True # dit zorgt dat de loop blijft lopen zolang de waarde niet veranderd
+    
+    while noExtentionProvided:
         
-    if inputImgName.lower().endswith(('.png', '.jpg', '.jpeg')):
         
-        inputCheck = True # dit zorgt dat de loop blijft lopen zolang de waarde niet veranderd
-        while inputCheck == True:
-            lenghtImg = len(inputImgName)
-            print(lenghtImg)
-            
-            
-            
-            msg = input("schrijf uw bericht: \n")
-            lenghtMsg = len(msg)
-            
-            print (lenghtImg, lenghtMsg )
-            return(lenghtImg, lenghtMsg)
+        
+        inputImgName = input("hoe heet het volledige bestand + extentie die je wil gebruiken: ")
+        
+        if inputImgName.lower().endswith(('.png', '.jpg', '.jpeg')): # Check filename, if ok:
+            noExtentionProvided = False
+        else:
+            print("dit was geen geldig bestands naam. bekijk het bestand in de verkenner")
+    
+    else:  
+        lenghtImg = len(inputImgName)
+        print(lenghtImg)
+        
+        msg = input("schrijf uw bericht: \n")
+        lenghtMsg = len(msg) * 8
+        
+        print (inputImgName, lenghtImg, msg, lenghtMsg )
+        return(lenghtImg, lenghtMsg)
+
+
 def decoding():
     inputCheck = False
-    print("dit was geen geldig bestands naam. bekijk het bestand in de verkenner")
+    
     
     return
     # lengtcaracters = len(inputImgName)
