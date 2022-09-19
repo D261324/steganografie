90bcc0e87d54497e11465680ed707f1203f701f9
stefano <72141994+D261324@users.noreply.github.com>
2022-09-19 11:33:00 +0200

    Revert "update main: basis function call (werkend)"
    This reverts commit 456fdfa1f2fed53c50a1833a615e245f5452e4a8.

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import re
-
-from methodes import encoding
-from methodes import decoding
-import methodes
-
 print('          __                                                         _____.__        ')
 print('  _______/  |_  ____   _________    ____   ____   ________________ _/ ____\__| ___   ')
 print(' /  ___/\   __\/ __ \ / ___\__  \  /    \ /  _ \ / ___\_  __ \__  \\   __\|  |/ __ \ ')
@@ -11,18 +5,3 @@ print(' \___ \  |  | \  ___// /_/  > __ \|   |  (  <_> ) /_/  >  | \// __ \|  | 
 print('/____  > |__|  \___  >___  (____  /___|  /\____/\___  /|__|  (____  /__|  |__|\___  >')
 print('     \/            \/_____/     \/     \/      /_____/            \/              \/ ')
 print('                                                                             by 23hz ')
-# askii art 
-# start program
-print('welkom bij de DE tool om jouw berichten te verstoppen in plain site')
-print("maak een keuze uit welke resource je wilt gebruiken: ")
-
-startKeuze = input("1. start met encoden \n2. start met decoderen\n maak een keuze: ")
-s =  int(startKeuze)
-
-if s == 1:
-    encoding()
-    
-    
-elif s == 2:
-    decoding()
-    
--- methodes.py
+++ methodes.py
@@ -1,34 +1,24 @@
 def encoding ():
     print("we starten het proces van encoderen...") 
-    
-    inputImgName = "blabla"
-    noExtentionProvided = True # dit zorgt dat de loop blijft lopen zolang de waarde niet veranderd
-    
-    while noExtentionProvided:
+    inputImgName = input("hoe heet het volledige bestand + extentie die je wil gebruiken: ")
         
+    if inputImgName.lower().endswith(('.png', '.jpg', '.jpeg')):
         
-        
-        inputImgName = input("hoe heet het volledige bestand + extentie die je wil gebruiken: ")
-        
-        if inputImgName.lower().endswith(('.png', '.jpg', '.jpeg')): # Check filename, if ok:
-            noExtentionProvided = False
-        else:
-            print("dit was geen geldig bestands naam. bekijk het bestand in de verkenner")
-    
-    else:  
-        lenghtImg = len(inputImgName)
-        print(lenghtImg)
-        
-        msg = input("schrijf uw bericht: \n")
-        lenghtMsg = len(msg) * 8
-        
-        print (inputImgName, lenghtImg, msg, lenghtMsg )
-        return(lenghtImg, lenghtMsg)
-
-
+        inputCheck = True # dit zorgt dat de loop blijft lopen zolang de waarde niet veranderd
+        while inputCheck == True:
+            lenghtImg = len(inputImgName)
+            print(lenghtImg)
+            
+            
+            
+            msg = input("schrijf uw bericht: \n")
+            lenghtMsg = len(msg)
+            
+            print (lenghtImg, lenghtMsg )
+            return(lenghtImg, lenghtMsg)
 def decoding():
     inputCheck = False
-    
+    print("dit was geen geldig bestands naam. bekijk het bestand in de verkenner")
     
     return
     # lengtcaracters = len(inputImgName)
