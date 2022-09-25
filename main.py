79f1cc2faa06a1e839301a92b6bc69b4c383e4d8
stefano <72141994+D261324@users.noreply.github.com>
2022-09-25 20:20:37 +0200

    update main: algemeen programma loopt ront
    werkt naar behoren. maybe nog een return functie

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import imp
+import re
+import methodes
+
 print('          __                                                         _____.__        ')
 print('  _______/  |_  ____   _________    ____   ____   ________________ _/ ____\__| ___   ')
 print(' /  ___/\   __\/ __ \ / ___\__  \  /    \ /  _ \ / ___\_  __ \__  \\   __\|  |/ __ \ ')
@@ -5,3 +9,22 @@ print(' \___ \  |  | \  ___// /_/  > __ \|   |  (  <_> ) /_/  >  | \// __ \|  | 
 print('/____  > |__|  \___  >___  (____  /___|  /\____/\___  /|__|  (____  /__|  |__|\___  >')
 print('     \/            \/_____/     \/     \/      /_____/            \/              \/ ')
 print('                                                                             by 23hz ')
+# askii art 
+# start program
+print('welkom bij de DE tool om jouw berichten te verstoppen in plain site')
+print("maak een keuze uit welke resource je wilt gebruiken: ")
+
+startKeuze = input("1. start met encoden \n2. start met decoderen\n maak een keuze: ")
+s =  int(startKeuze)
+
+if s == 1:
+    methodes.encoding()
+    
+    
+elif s == 2:
+    methodes.decoding()
+    
+# 1. veranderingen doorlezen 
+# 2. van losse werkende oplossingen een commit maken en omschrijven
+# 3. her op pakken main
+    
