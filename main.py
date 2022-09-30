c2edeae93046c49cd537a0aadc64867971eb8de3
stefano <72141994+D261324@users.noreply.github.com>
2022-09-30 15:34:46 +0200

    last update before demo: p.o.c
    kleine toevoegingen zoals clear screen die het scherm na een keuze leeg maakt.
    en een loop om de main() function heen zodat de gebruiker na een proces de keuze heeft om iets anders te doen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import imp
-import re
+#imports van benodigde library's 
 import methodes
+import os
 
+# askii art 
 print('          __                                                         _____.__        ')
 print('  _______/  |_  ____   _________    ____   ____   ________________ _/ ____\__| ___   ')
 print(' /  ___/\   __\/ __ \ / ___\__  \  /    \ /  _ \ / ___\_  __ \__  \\   __\|  |/ __ \ ')
@@ -9,22 +10,27 @@ print(' \___ \  |  | \  ___// /_/  > __ \|   |  (  <_> ) /_/  >  | \// __ \|  | 
 print('/____  > |__|  \___  >___  (____  /___|  /\____/\___  /|__|  (____  /__|  |__|\___  >')
 print('     \/            \/_____/     \/     \/      /_____/            \/              \/ ')
 print('                                                                             by 23hz ')
-# askii art 
 # start program
 print('welkom bij de DE tool om jouw berichten te verstoppen in plain site')
 print("maak een keuze uit welke resource je wilt gebruiken: ")
 
-startKeuze = input("1. start met encoden \n2. start met decoderen\n maak een keuze: ")
-s =  int(startKeuze)
+running = True
+while(running):
+    startKeuze = input("1. start met encoden \n2. start met decoderen\n maak een keuze: ")
+    s =  int(startKeuze)
+    os.system('clear')
 
-if s == 1:
-    methodes.encoding()
-    
-    
-elif s == 2:
-    methodes.decoding()
-    
-# 1. veranderingen doorlezen 
-# 2. van losse werkende oplossingen een commit maken en omschrijven
-# 3. her op pakken main
-    
+    if s == 1:
+        methodes.encoding()
+        #methodes.modPix()
+        
+    elif s == 2:
+        methodes.decoding()
+        
+    else:
+        print("dit was geen geldige keuze")
+        
+    # 1. veranderingen doorlezen 
+    # 2. van losse werkende oplossingen een commit maken en omschrijven
+    # 3. her op pakken main    
+# ======================== over bodige code =======================
