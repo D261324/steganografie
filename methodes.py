dc5e2e2787cd279c17e43c8447d9af27c3a6558b
stefano <72141994+D261324@users.noreply.github.com>
2022-09-30 15:32:25 +0200

    last update before demo: p.o.c
    last minute extra code toegevoegd en goed gezet. er gingen een aantal dingetjes niet lekker en die moesten er uit. nieuwe functie gevonden en aan gepast naar behoefte

--- methodes.py
+++ methodes.py
@@ -2,10 +2,90 @@ import os
 import types
 from pkgutil import ImpImporter
 from re import I
-from PIL import Image 
-
-#import cv2
+from PIL import Image
 import numpy as np
+
+def modPix(pix, data):
+ 
+
+    datalist = msgToBinairy(data)
+
+    lendata = len(datalist)
+
+    imdata = iter(pix)
+ 
+
+    for i in range(lendata):
+ 
+
+        # Extracting 3 pixels at a time
+
+        pix = [value for value in imdata.__next__()[:3] +
+
+                                imdata.__next__()[:3] +
+
+                                imdata.__next__()[:3]]
+ 
+
+        # Pixel value should be made
+
+        # odd for 1 and even for 0
+
+        for j in range(0, 8):
+
+            if (datalist[i][j] == '0' and pix[j]% 2 != 0):
+
+                pix[j] -= 1
+ 
+
+            elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
+
+                if(pix[j] != 0):
+
+                    pix[j] -= 1
+
+                else:
+
+                    pix[j] += 1
+
+                # pix[j] -= 1
+ 
+
+        # Eighth pixel of every set tells
+
+        # whether to stop ot read further.
+
+        # 0 means keep reading; 1 means thec
+
+        # message is over.
+
+        if (i == lendata - 1):
+
+            if (pix[-1] % 2 == 0):
+
+                if(pix[-1] != 0):
+
+                    pix[-1] -= 1
+
+                else:
+
+                    pix[-1] += 1
+ 
+
+        else:
+
+            if (pix[-1] % 2 != 0):
+
+                pix[-1] -= 1
+ 
+
+        pix = tuple(pix)
+
+        yield pix[0:3]
+
+        yield pix[3:6]
+
+        yield pix[6:9]
 
 
 def encoding ():
@@ -16,7 +96,6 @@ def encoding ():
     while noExtentionProvided:
         # img search start
             inputImgName = input("hoe heet het volledige bestand + extentie die je wil gebruiken: ")
-            findImgFile = False # zorgt er voor dat de loop blijft lopen tot dat de variable op true wordt gezet
             
             dirFilePath = os.path.exists("C:\\Users\\stefa\\OneDrive\\Desktop\\software developer\\td4-ssd-3b 2022-2023\\project_extern\\project 1\\fase 3 development\\steganografie\\img\\" + inputImgName)
             if dirFilePath == True:
@@ -34,12 +113,14 @@ def encoding ():
     # img search end
     else:  
         lenghtImg = len(inputImgName)
+        inputImgNameInBin = list(msgToBinairy(inputImgName))
         print(lenghtImg)
         
         msg = input("schrijf uw bericht: \n")
         encodedMsg = msgToBinairy(msg)
-        inputImgNameInBin = list(msgToBinairy(inputImgName))
+        
         binListMsg = list(encodedMsg)
+        lenghtMsg = len(binListMsg)
         
 
         print(binListMsg)
@@ -47,27 +128,27 @@ def encoding ():
         print(inputImgNameInBin)
         print(len(inputImgNameInBin))
         
-        #print (inputImgName, lenghtImg, msg, lenghtMsg )
-        #return(lenghtImg, lenghtMsg)
+        print (inputImgName, lenghtImg, msg, lenghtMsg )
+        return(lenghtImg, lenghtMsg, binListMsg)
 
-
+#optie 2
 def decoding():
-    inputCheck = False
+    imgNameInput = input("Enter image name(with extension) : ")
+    img = Image.open("C:\\Users\\stefa\\OneDrive\\Desktop\\software developer\\td4-ssd-3b 2022-2023\\project_extern\\project 1\\fase 3 development\\steganografie\\img\\" + imgNameInput) 
+    data = ''
+    imgdata = iter(img.getdata())
     
-    # lengtcaracters = len(inputImgName)
-    # print(lengtcaracters)
+    while (True):
+        pixels = [value for value in imgdata.next()[:3] +
+                                imgdata.next()[:3] +
+                                imgdata.next()[:3]]
+        print(pixels)
+        print(len(pixels))
+        print("+++++++++++++++++++++++++++++++++++++++++++++++")
+       
+ 
     
-#def showImg(imgFile):
-    cv2.imshow(imgFile) #werkt nog niet 
-    
-#def useablePixelSize():
-    pixelSize = 24
 
-#def charToAsciiValue(msg):
-    unicode_value = [ord(x) for x in msg]
-    
-    
-    
 # msgTobinairy start; deze methode zet de caracters om naar binairy 
 def msgToBinairy(msg):
     # voor string
@@ -83,8 +164,17 @@ def msgToBinairy(msg):
     else:
         return TypeError(" invoer type wordt niet ondersteund")
 # msgTobinairy end;
-# msg = "hioi"
-# countValue = len(somMsgToBinairy(msg))
+
+def payloadSom(inputImgNameInBin, binListMsg):
+    payloadFile = print(len(inputImgNameInBin))
+    payloadMsg = print(len(binListMsg))
+    print(payloadFile)
+    print(payloadMsg)
+    
+# ======================== over bodige code =======================
+    # msg = "hioi"
+# countValue = len(msgToBinairy(msgToBinairy))
     
 # print(countValue)
-# print(somMsgToBinairy(msg))
+# print(msgToBinairy(binListMsg))
+    
